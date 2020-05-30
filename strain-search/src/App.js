518b1244f1697ab42417ee20dc0305da9c681932
Justin Durocher <justduro@gmail.com>
2020-05-30 12:11:20 -0400

    ready

---
+++ strain-search/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import {BrowserRouter , Route , Switch} from "react-router-dom";
+
+import Welcome from './components/Welcome.js'
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    
+      <Switch>
+
+        <Route path="/" component={Welcome}></Route>
+
+      </Switch>
+
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ strain-search/src/components/Welcome.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+import './components/css/welcome.css';
+
+class welcome extends Component{
+
+    render(){
+
+        return(
+
+            <div className='welcomePage'>
+
+                
+
+            </div>
+
+        )
+
+    }
+
+}
